d965f3f7422e08de9c77a72638c67bd43ecb5a49
JJIWON <hello093093@gmail.com>
2024-06-17 16:19:52 +0900

    2024/06/17

---
+++ src/main/java/servlet/Servlet01.java
@@ -0,0 +1,34 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/01")
+public class Servlet01 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//	표현할 데이터 속성으로 설정 -> jsp에서 받아와서 JSP, EL 출력
+		int iValue = 2024;
+		double fValue = Math.PI;
+		String sValue = "Hello Servlet";
+		boolean bValue = true;
+		Object nullValue = null;
+		
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/01.jsp");
+		rd.forward(request, response);
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
